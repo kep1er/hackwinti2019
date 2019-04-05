cdcafa6d4b7da2a2dcf47f428c6225059ce0be78
David Schlebusch <david.schlebusch@students.fhnw.ch>
2019-04-05 22:35:08 +0200

    Extended readme with angularFire.

--- ng/src/app/app.module.ts
+++ ng/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire';
 import { environment } from '../environments/environment';
 
-
 @NgModule({
   declarations: [
     AppComponent

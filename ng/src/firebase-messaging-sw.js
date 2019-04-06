47e0581134d196ffcb4736815cefdf2980c88ae0
David Schlebusch <david.schlebusch@students.fhnw.ch>
2019-04-06 19:24:09 +0200

    Added messaging part 2.

--- ng/src/firebase-messaging-sw.js
+++ ng/src/firebase-messaging-sw.js
@@ -1,6 +1,6 @@
 importScripts('https://www.gstatic.com/firebasejs/4.6.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/4.6.1/firebase-messaging.js');
 firebase.initializeApp({
-    'messagingSenderId': 'YOUR_UNIQUE_SENDER_ID'
+    'messagingSenderId': '961664874246'
 });
 const messaging = firebase.messaging();

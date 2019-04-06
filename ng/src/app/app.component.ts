7cb1388566864cf48cd7091e006367b158ce0ef7
schaumic <schaumic@students.zhaw.ch>
2019-04-06 09:32:20 +0200

    frontend updates

--- ng/src/app/app.component.ts
+++ ng/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import {AuthService} from "./services/auth/auth.service";
 import {ChatService} from "./services/chat/chat.service";
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +11,5 @@ import {ChatService} from "./services/chat/chat.service";
 })
 export class AppComponent {
   title = 'hackwinti2019';
-  constructor(public auth: AuthService, public cs: ChatService) {}
+  constructor(public auth: AuthService, public cs: ChatService, public location: Location, public router: Router) {}
 }

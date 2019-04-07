b8b7f1ee92edfc02fcc6a4255f065ee587342de1
schaumic <schaumic@students.zhaw.ch>
2019-04-07 10:50:31 +0200

    image now random and filtered

--- ng/src/app/components/chat-image-selection/chat-image-selection.component.ts
+++ ng/src/app/components/chat-image-selection/chat-image-selection.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AngularFirestore} from "@angular/fire/firestore";
 import {map} from "rxjs/operators";
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
     selector: 'app-chat-image-selection',
@@ -11,18 +12,24 @@ export class ChatImageSelectionComponent implements OnInit {
 
     constructor(
         private afs: AngularFirestore,
+        private route: ActivatedRoute,
     ) {
     }
 
     images = [];
 
     ngOnInit() {
+        const chatId = this.route.snapshot.paramMap.get('id');
         this.afs.collection('photos').get().subscribe(
             res => {
                 this.images = res.docs.map(doc => doc.data());
-                this.images.sort(() => Math.random() - 0.5);
-                this.images = this.images.slice(0, 3);
-                console.log(this.images);
+                this.images = this.images
+                    .filter((img) => {
+                        console.log(chatId, img.chatId, img.chatId !== chatId)
+                        return img.chatId !== chatId;
+                    })
+                    .sort(() => Math.random() - 0.5)
+                    .slice(0, 4);
             }
         );
     }
